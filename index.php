17d9b2f9a60a83003e6c2edaba37cf1ffa6a1033
Teemu Taskula <teemu.taskula@aalto.fi>
2014-10-19 19:22:57 +0300

    test

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 
 <?php include 'header.php'; ?>
 
-<h1>Flying Cameras Prototyyppi</h1>
-<p>Aletaan rakentamaan prototyyppi sivustoa tänne.</p>
+<div id="home-page">
+	<h1>Flying Cameras Prototyyppi</h1>
+	<p>Aletaan rakentamaan prototyyppi sivustoaaaaaa tänne.</p>
+</div>
 
 <?php include 'footer.php'; ?>
